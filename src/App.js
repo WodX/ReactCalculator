cc6e6f383cad72cbad7170b3a1a2a66b26b0cfe8
Andre Nunes <a.sousa@runtime-revolution.com>
2021-05-13 10:06:24 +0100

    Add numbers to the display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+const Button = ({value, className, onClick}) => {
+  return (
+    <div className={`button ${className}`} onClick={() => onClick(value)}><button>{value}</button></div>
+  )
+}
+
+const Panel = ({onClick}) => {
+  return (
+    <div className="button-panel">
+      <div>
+          <Button value="AC" onClick={onClick} />
+          <Button value="+/-" onClick={onClick} />
+          <Button value="%" onClick={onClick} />
+          <Button value="÷" className="orange" onClick={onClick} />
+      </div>
+      <div>
+          <Button value="7" onClick={onClick} />
+          <Button value="8" onClick={onClick} />
+          <Button value="9" onClick={onClick} />
+          <Button value="x" className="orange" onClick={onClick} />
+      </div>
+      <div>
+          <Button value="4" onClick={onClick} />
+          <Button value="5" onClick={onClick} />
+          <Button value="6" onClick={onClick} />
+          <Button value="-" className="orange" onClick={onClick} />
+      </div>
+      <div>
+          <Button value="1" onClick={onClick} />
+          <Button value="2" onClick={onClick} />
+          <Button value="3" onClick={onClick} />
+          <Button value="+" className="orange" onClick={onClick} />
+      </div>
+      <div>
+          <Button value="0" className="wide" onClick={onClick} />
+          <Button value="." onClick={onClick} />
+          <Button value="=" className="orange" onClick={onClick} />
+      </div>
+    </div>
+  );
+}
+
+const Display = ({text}) => {
+  return (
+    <div className="display">
+      <div>{text}</div>
+    </div>
+  );
+}
 
 function App() {
+const [text, setText] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+        <div className="app">
+            <Display text={text}/>
+            <Panel onClick={(value) => setText(value)}/>
+        </div>
     </div>
   );
 }
